f99e238faccfdff19689c0db9ae813487795965a
Cristian Silva <techcarebz@gmail.com>
2017-09-07 15:19:53 -0600

    Changing Returning data to single argument. Looping through all articles returned from rss and files

--- flaskprj01.py
+++ flaskprj01.py
@@ -14,8 +14,8 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article=feed['entries'][0]
-    return render_template("home.html", article=first_article)
+    #first_article=feed['entries'][0]
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run()

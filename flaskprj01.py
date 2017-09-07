5b527fe4a0d56c842bf8360f6354bbc5d335ed40
Cristian Silva <techcarebz@gmail.com>
2017-09-07 13:52:02 -0600

    Adding Jinja Templating to file and passing data.

--- flaskprj01.py
+++ flaskprj01.py
@@ -15,17 +15,10 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article=feed['entries'][0]
-    return """
-    <html>
-    <body>
-    <h1>Headlines</h1>
-    <b>{0}</b><br/>
-    <i>{1}</i><br/>
-    <p>{2}</p><br/>
-    </body>
-    </html>
-    """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
-
+    return render_template("home.html",
+                           title=first_article.get("title"),
+                           published=first_article.get("published"),
+                           summary=first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run()
@@ -58,3 +51,20 @@ if __name__ == '__main__':
 #
 # if __name__ == '__main__':
 #     app.run()
+
+#Version2
+# @app.route("/")
+# @app.route("/<publication>")
+# def get_news(publication="bbc"):
+#     feed = feedparser.parse(RSS_FEEDS[publication])
+#     first_article=feed['entries'][0]
+#     return """
+#     <html>
+#     <body>
+#     <h1>Headlines</h1>
+#     <b>{0}</b><br/>
+#     <i>{1}</i><br/>
+#     <p>{2}</p><br/>
+#     </body>
+#     </html>
+#     """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))

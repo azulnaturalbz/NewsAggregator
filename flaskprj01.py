d02bd5be98cc906f34cb3d6db9b887cebcce6bdb
Cristian Silva <techcarebz@gmail.com>
2017-09-10 11:17:58 -0600

    Fully completed page without cosmetic touch up. Changed currency information to be more dynamic.

--- flaskprj01.py
+++ flaskprj01.py
@@ -5,42 +5,45 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn':'http://rss.cnn.com/rss/edition.rss',
-              'fox':'http://feeds.foxnews.com/foxnews/latest',
-                'iol':'http://www.iol.co.za/cmlink/1.640'}
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-DEFAULTS = {'publication':'bbc',
-            'city':'Belize,bz',
-            'currency_from':'BZD',
-            'currency_to':'USD'}
+DEFAULTS = {'publication': 'bbc',
+            'city': 'Belize,bz',
+            'currency_from': 'BZD',
+            'currency_to': 'USD'}
 
-WEATHER_URL ="http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=f47aeab3995ca500141e77a82f634ba6"
-CURRENCY_URL ="https://openexchangerates.org//api/latest.json?app_id=3939e9bcc6ab47719d6da46537c431e6"
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=f47aeab3995ca500141e77a82f634ba6"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=3939e9bcc6ab47719d6da46537c431e6"
+
 
 @app.route("/")
 def home():
-    #get cust headlines , based on user input or default
+    # get cust headlines , based on user input or default
     publication = request.args.get("publication")
     if not publication:
         publication = DEFAULTS['publication']
-    articles= get_news(publication)
-    #get cust weather based on user input or default
+    articles = get_news(publication)
+    # get cust weather based on user input or default
     city = request.args.get('city')
-    if not city :
+    if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    #get customized currency from user input or default
+    # get customized currency from user input or default
     currency_from = request.args.get("currency_from")
     if not currency_from:
         currency_from = DEFAULTS['currency_from']
     currency_to = request.args.get("currency_to")
     if not currency_to:
         currency_to = DEFAULTS['currency_to']
-    rate, currencies = get_rate(currency_from,currency_to)
-    return render_template("home.html",articles=articles,weather=weather,currency_from=currency_from,currency_to=currency_to,rate=rate,currencies=sorted(currencies))
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles, weather=weather, currency_from=currency_from,
+                           currency_to=currency_to, rate=rate, currencies=sorted(currencies))
 
 
 def get_news(query):
@@ -60,25 +63,26 @@ def get_weather(query):
     parsed = json.loads(data)
     weather = None
     if parsed.get("weather"):
-        weather = {"description":parsed["weather"][0]["description"],
-                   "temperature":parsed["main"]["temp"],
-                   "city":parsed["name"],
-                   "country":parsed['sys']['country']}
+        weather = {"description": parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"],
+                   "country": parsed['sys']['country']}
     return weather
 
-def get_rate(frm,to):
+
+def get_rate(frm, to):
     all_currency = urllib2.urlopen(CURRENCY_URL).read()
     parsed = json.loads(all_currency).get('rates')
     frm_rate = parsed.get(frm.upper())
     to_rate = parsed.get(to.upper())
-    return to_rate/frm_rate,parsed.keys()
+    return to_rate / frm_rate, parsed.keys()
+
 
 if __name__ == '__main__':
     app.run()
 
 
-
-##Version 1
+#Version 1
 # import feedparser
 # from flask import Flask
 #
@@ -105,7 +109,7 @@ if __name__ == '__main__':
 # if __name__ == '__main__':
 #     app.run()
 
-#Version2
+# Version2
 # @app.route("/")
 # @app.route("/<publication>")
 # def get_news(publication="bbc"):

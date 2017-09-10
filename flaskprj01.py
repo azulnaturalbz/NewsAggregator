f0a1c49e8b01a1e15b3260198830907db052ca0c
Cristian Silva <techcarebz@gmail.com>
2017-09-10 11:06:09 -0600

    Fixing Currency from being static to fetch all from api.

--- flaskprj01.py
+++ flaskprj01.py
@@ -13,11 +13,16 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
                 'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 DEFAULTS = {'publication':'bbc',
-            'city':'Belize,bz'}
+            'city':'Belize,bz',
+            'currency_from':'BZD',
+            'currency_to':'USD'}
+
+WEATHER_URL ="http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=f47aeab3995ca500141e77a82f634ba6"
+CURRENCY_URL ="https://openexchangerates.org//api/latest.json?app_id=3939e9bcc6ab47719d6da46537c431e6"
 
 @app.route("/")
 def home():
-
+    #get cust headlines , based on user input or default
     publication = request.args.get("publication")
     if not publication:
         publication = DEFAULTS['publication']
@@ -27,7 +32,15 @@ def home():
     if not city :
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html",articles=articles,weather=weather)
+    #get customized currency from user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from,currency_to)
+    return render_template("home.html",articles=articles,weather=weather,currency_from=currency_from,currency_to=currency_to,rate=rate,currencies=sorted(currencies))
 
 
 def get_news(query):
@@ -49,9 +62,16 @@ def get_weather(query):
     if parsed.get("weather"):
         weather = {"description":parsed["weather"][0]["description"],
                    "temperature":parsed["main"]["temp"],
-                   "city":parsed["name"]}
+                   "city":parsed["name"],
+                   "country":parsed['sys']['country']}
     return weather
 
+def get_rate(frm,to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate/frm_rate,parsed.keys()
 
 if __name__ == '__main__':
     app.run()

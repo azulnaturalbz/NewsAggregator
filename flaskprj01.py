3775aa88a7293cf57bb1b54b6c57f7e774f4947f
Cristian Silva <techcarebz@gmail.com>
2017-09-12 11:01:19 -0600

    Well belize news version of project. With added information appended.

--- flaskprj01.py
+++ flaskprj01.py
@@ -10,12 +10,14 @@ from flask import request
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+RSS_FEEDS = {'bbn': 'https://www.breakingbelizenews.com/feed/',
+             'lov': 'http://lovefm.com/feed/',
+             'amd': 'http://amandala.com.bz/news/feed/',
+             'sps': 'https://www.sanpedrosun.com/feed/',
+             'rpt': 'http://www.reporter.bz/feed/',
+             'mybz':'http://www.mybelize.net/feed/'}
 
-DEFAULTS = {'publication': 'bbc',
+DEFAULTS = {'publication': 'bbn',
             'city': 'Belize,bz',
             'currency_from': 'BZD',
             'currency_to': 'USD'}

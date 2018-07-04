ba5dac6ed3d6740bd2f7b462db26fc10086d07a0
Cristian Silva <dev@silvatech.org>
2018-07-04 09:51:02 -0500

    Update flaskprj01.py

--- flaskprj01.py
+++ flaskprj01.py
@@ -22,8 +22,8 @@ DEFAULTS = {'publication': 'bbn',
             'currency_from': 'BZD',
             'currency_to': 'USD'}
 
-WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=f47aeab3995ca500141e77a82f634ba6"
-CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=3939e9bcc6ab47719d6da46537c431e6"
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=addyourownappid"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=addyourownappid"
 
 
 def get_value_with_fallback(key):
@@ -39,32 +39,13 @@ def home():
 # get cust headlines , based on user input or default
     publication = get_value_with_fallback("publication")
     articles=get_news(publication)
-#    publication = request.args.get("publication")
-#    if not publication:
-#        publication = request.cookies.get("publication")
-#        if not publication:
-#            publication = DEFAULTS['publication']
-#    articles = get_news(publication)
 # get cust weather based on user input or default
     city = get_value_with_fallback("city")
     weather = get_weather(city)
-#    city = request.args.get('city')
-#    if not city:
-#        city = DEFAULTS['city']
-#    weather = get_weather(city)
 # get customized currency from user input or default
     currency_from = get_value_with_fallback("currency_from")
     currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
-#    currency_from = request.args.get("currency_from")
-#   if not currency_from:
-#       currency_from = DEFAULTS['currency_from']
-#   currency_to = request.args.get("currency_to")
-#    if not currency_to:
-#        currency_to = DEFAULTS['currency_to']
-#    rate, currencies = get_rate(currency_from, currency_to)
-
-    #return render_template("home.html", articles=articles, weather=weather, currency_from=currency_from,currency_to=currency_to, rate=rate, currencies=sorted(currencies))
     response = make_response(render_template("home.html", articles=articles, weather=weather, currency_from=currency_from,currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
     expires = datetime.datetime.now() + datetime.timedelta(days=365)
     response.set_cookie("publication",publication,expires=expires)
@@ -73,6 +54,7 @@ def home():
     response.set_cookie("currency_to",currency_to,expires=expires)
     return response
 
+  
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
         publication = DEFAULTS["publication"]
@@ -107,48 +89,3 @@ def get_rate(frm, to):
 
 if __name__ == '__main__':
     app.run()
-
-
-#Version 1
-# import feedparser
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-#
-# BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
-#
-# @app.route('/')
-# def get_news():
-#     feed = feedparser.parse(BBC_FEED)
-#     first_article = feed['entries'][0]
-#     return """
-#     <html>
-#     <body>
-#     <h1>BBC Headlines</h1>
-#     <b>{0}</b><br/>
-#     <i>{1}</i><br/>
-#     <p>{2}</p><br/>
-#     </body>
-#     <html>
-#     """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
-#
-# if __name__ == '__main__':
-#     app.run()
-
-# Version2
-# @app.route("/")
-# @app.route("/<publication>")
-# def get_news(publication="bbc"):
-#     feed = feedparser.parse(RSS_FEEDS[publication])
-#     first_article=feed['entries'][0]
-#     return """
-#     <html>
-#     <body>
-#     <h1>Headlines</h1>
-#     <b>{0}</b><br/>
-#     <i>{1}</i><br/>
-#     <p>{2}</p><br/>
-#     </body>
-#     </html>
-#     """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))

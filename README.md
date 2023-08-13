95571db37068a5a049645aef67827fe035e3ecbf
Cristian Silva <chrissilva9420@gmail.com>
2023-08-13 09:46:42 -0600

    1) Added README.md 2) Updated packages and updated requirements.txt 3) added .env example. 4) Docker and Docker-compose 5) Refactored code.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Newsagg
+
+## Initial Setup
+
+### 1. Clone the Repository
+
+To clone the repository, run the following commands:
+
+```bash
+git clone [repository_url]
+cd [repository_directory]
+```
+
+### 2. Set Environment Variables
+
+
+
+### 3. Run the Application
+
+To run the application, use Docker Compose:
+
+```bash
+docker-compose up --build -d
+```
--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
+import datetime
 import json
+import os
 from urllib.request import urlopen
 from urllib.parse import quote
-import feedparser
-import datetime
-from flask import make_response
-from flask import Flask
-from flask import render_template
-from flask import request
 
-app = Flask(__name__)
+from dotenv import load_dotenv
+import feedparser
+from flask import Flask, make_response, render_template, request
+
+# Load environment variables
+load_dotenv()
+
+# Constants
+WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
+WEATHER_URL = f"http://api.openweathermap.org/data/2.5/weather?q={{}}&units=metric&APPID={WEATHER_API_KEY}"
+
+DEFAULTS = {
+  'publication': 'breaking-belize-news',
+  'city': 'Belize,bz',
+  'currency_from': 'BZD',
+  'currency_to': 'USD'
+}
 
 RSS_FEEDS = {'breaking-belize-news': 'https://www.breakingbelizenews.com/feed/',
              'channel-7':'http://www.7newsbelize.com/rss.php',
@@ -18,62 +30,56 @@ RSS_FEEDS = {'breaking-belize-news': 'https://www.breakingbelizenews.com/feed/',
              'san-pedro-sun': 'https://www.sanpedrosun.com/feed/',
              'reporter': 'http://www.reporter.bz/feed/',
              'mybelize': 'http://www.mybelize.net/feed/'}
-
-DEFAULTS = {'publication': 'breaking-belize-news',
-            'city': 'Belize,bz',
-            'currency_from': 'BZD',
-            'currency_to': 'USD'}
-
-WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=1f3d91f00155c1f35b07c2237891ded0"
+app = Flask(__name__)
 
 
-def get_value_with_fallback(key):
-    if request.args.get(key):
-        return request.args.get(key)
-    if request.cookies.get(key):
-        return request.cookies.get(key)
-    return DEFAULTS[key]
+def get_news(publication):
+  """Fetch news for a specific publication."""
+  feed = feedparser.parse(RSS_FEEDS.get(publication, DEFAULTS["publication"]))
+  return feed['entries']
+
+
+def get_weather(city):
+  """Fetch weather information for a specific city."""
+  query = quote(city)
+  url = WEATHER_URL.format(query)
+  try:
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    if parsed.get("weather"):
+      return {
+        "description": parsed["weather"][0]["description"],
+        "temperature": parsed["main"]["temp"],
+        "city": parsed["name"],
+        "country": parsed['sys']['country']
+      }
+  except Exception as e:
+    print(f"Error fetching weather: {e}")
+  return {}
 
 
 @app.route("/")
 def home():
-    # get cust headlines , based on user input or default
-    publication = get_value_with_fallback("publication")
-    articles = get_news(publication)
-# get cust weather based on user input or default
-    city = get_value_with_fallback("city")
-    weather = get_weather(city)
-# get customized currency from user input or default
-    response = make_response(render_template(
-        "home.html", articles=articles, weather=weather, options=RSS_FEEDS, publication=publication))
-    expires = datetime.datetime.now() + datetime.timedelta(days=365)
-    response.set_cookie("publication", publication, expires=expires)
-    response.set_cookie("city", city, expires=expires)
-    return response
+  """Home route to display news and weather."""
+  publication = get_value_with_fallback("publication")
+  city = get_value_with_fallback("city")
+
+  articles = get_news(publication)
+  weather = get_weather(city)
+
+  response = make_response(render_template(
+    "home.html", articles=articles, weather=weather, options=RSS_FEEDS, publication=publication
+  ))
+  expires = datetime.datetime.now() + datetime.timedelta(days=365)
+  response.set_cookie("publication", publication, expires=expires)
+  response.set_cookie("city", city, expires=expires)
+  return response
 
 
-def get_news(query):
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = DEFAULTS["publication"]
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    return feed['entries']
-
-
-def get_weather(query):
-    query = quote(query)
-    url = WEATHER_URL.format(query)
-    data = urlopen(url).read()
-    parsed = json.loads(data)
-    weather = None
-    if parsed.get("weather"):
-        weather = {"description": parsed["weather"][0]["description"],
-                   "temperature": parsed["main"]["temp"],
-                   "city": parsed["name"],
-                   "country": parsed['sys']['country']}
-    return weather
+def get_value_with_fallback(key):
+  """Get value from request args, cookies or defaults."""
+  return request.args.get(key) or request.cookies.get(key) or DEFAULTS[key]
 
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
